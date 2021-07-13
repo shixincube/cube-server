182fab6dd5128893d9e91e69b31afd0e54b455c0
Ambrose Xu <xujiangwei@spap.com>
2021-07-13 13:45:45 +0800

    Update stream storage

--- dispatcher/src/cube/dispatcher/signal/StreamStorage.java
+++ dispatcher/src/cube/dispatcher/signal/StreamStorage.java
@@ -26,6 +26,14 @@
 
 package cube.dispatcher.signal;
 
+import cell.core.talk.PrimitiveInputStream;
+import cell.util.collection.FlexibleByteBuffer;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * 流存储器。
  */
@@ -33,13 +41,28 @@ public class StreamStorage {
 
     private final static StreamStorage instance = new StreamStorage();
 
-    private StreamStorage() {
+    private Map<String, List<FlexibleByteBuffer>> bufferMap;
 
+    private StreamStorage() {
+        this.bufferMap = new ConcurrentHashMap<>();
     }
 
     public static StreamStorage getInstance() {
         return StreamStorage.instance;
     }
 
+    public void process(PrimitiveInputStream inputStream) {
+        FlexibleByteBuffer buf = new FlexibleByteBuffer();
+        try {
+            int length = 0;
+            byte[] bytes = new byte[2048];
+            while ((length = inputStream.read(bytes)) > 0) {
+                buf.put(bytes, 0, length);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+
+    }
 }
