ffee4c2f9873f3136598fd8490662dfc22d477a7
Ambrose Xu <xujiangwei@spap.com>
2022-02-12 20:53:27 +0800

    Update hocr file

--- service-fileprocessor/src/cube/service/fileprocessor/util/TesseractHocrFile.java
+++ service-fileprocessor/src/cube/service/fileprocessor/util/TesseractHocrFile.java
@@ -27,9 +27,13 @@
 package cube.service.fileprocessor.util;
 
 import cell.util.Utils;
+import cube.common.JSONable;
 import cube.geometry.BoundingBox;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
@@ -37,11 +41,15 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  */
-public class TesseractHocrFile {
+public class TesseractHocrFile implements JSONable {
+
+    private List<Page> pages = new ArrayList<>();
 
     public TesseractHocrFile(File file) {
         File newFile = this.preproccess(file);
@@ -49,6 +57,28 @@ public class TesseractHocrFile {
             this.readXML(newFile);
             newFile.delete();
         }
+    }
+
+    public List<Page> getPages() {
+        return this.pages;
+    }
+
+    @Override
+    public JSONObject toJSON() {
+        return this.toCompactJSON();
+    }
+
+    @Override
+    public JSONObject toCompactJSON() {
+        JSONObject json = new JSONObject();
+
+        JSONArray pageArray = new JSONArray();
+        for (Page page : this.pages) {
+            pageArray.put(page.toJSON());
+        }
+        json.put("pages", pageArray);
+
+        return json;
     }
 
     private File preproccess(File file) {
@@ -114,7 +144,10 @@ public class TesseractHocrFile {
                 Element nodeEl = (Element) nodeList.item(i);
                 String className = nodeEl.getAttribute("class");
                 if (className.equals("ocr_page")) {
-                    parsePage(nodeEl);
+                    Page page = parsePage(nodeEl);
+                    if (null != page) {
+                        this.pages.add(page);
+                    }
                 }
             }
         } catch (ParserConfigurationException e) {
@@ -144,29 +177,215 @@ public class TesseractHocrFile {
                             Integer.parseInt(bboxParam[4]));
                     page.bbox = bbox;
                 }
+                break;
             }
         }
 
         NodeList nodeList = el.getChildNodes();
+        for (int i = 0; i < nodeList.getLength(); ++i) {
+            Node node = nodeList.item(i);
+            if (Node.ELEMENT_NODE == node.getNodeType()) {
+                Element child = (Element) node;
+                String className = child.getAttribute("class");
+                if (className.equals("ocr_carea")) {
+                    this.parseArea(page, child);
+                }
+            }
+        }
 
         return page;
     }
 
-    private void parseArea(Page page, Element el) {
+    private Area parseArea(Page page, Element el) {
+        Area area = new Area();
 
+        String title = el.getAttribute("title");
+        String[] params = title.trim().split(" ");
+        BoundingBox bbox = new BoundingBox(Integer.parseInt(params[1]),
+                Integer.parseInt(params[2]),
+                Integer.parseInt(params[3]),
+                Integer.parseInt(params[4]));
+        area.bbox = bbox;
+        page.areas.add(area);
+
+        NodeList nodeList = el.getChildNodes();
+        for (int i = 0; i < nodeList.getLength(); ++i) {
+            Node node = nodeList.item(i);
+            if (Node.ELEMENT_NODE == node.getNodeType()) {
+                Element child = (Element) node;
+                String className = child.getAttribute("class");
+                if (className.equals("ocr_par")) {
+                    this.parsePart(area, child);
+                }
+            }
+        }
+
+        return area;
+    }
+
+    private Part parsePart(Area area, Element el) {
+        Part part = new Part();
+
+        part.language = el.getAttribute("lang");
+
+        String title = el.getAttribute("title");
+        String[] params = title.trim().split(" ");
+        BoundingBox bbox = new BoundingBox(Integer.parseInt(params[1]),
+                Integer.parseInt(params[2]),
+                Integer.parseInt(params[3]),
+                Integer.parseInt(params[4]));
+        part.bbox = bbox;
+        area.parts.add(part);
+
+        NodeList nodeList = el.getChildNodes();
+        for (int i = 0; i < nodeList.getLength(); ++i) {
+            Node node = nodeList.item(i);
+            if (Node.ELEMENT_NODE == node.getNodeType()) {
+                Element child = (Element) node;
+                String className = child.getAttribute("class");
+                if (className.equals("ocr_line")) {
+                    this.parseLine(part, child);
+                }
+            }
+        }
+
+        return part;
+    }
+
+    private Line parseLine(Part part, Element el) {
+        Line line = new Line();
+
+        String title = el.getAttribute("title");
+        String[] params = title.trim().split(";");
+        for (String param : params) {
+            param = param.trim();
+            if (param.startsWith("bbox")) {
+                String[] bbParams = param.split(" ");
+                if (bbParams.length == 5) {
+                    BoundingBox bbox = new BoundingBox(Integer.parseInt(bbParams[1]),
+                            Integer.parseInt(bbParams[2]),
+                            Integer.parseInt(bbParams[3]),
+                            Integer.parseInt(bbParams[4]));
+                    line.bbox = bbox;
+                }
+
+                break;
+            }
+        }
+
+        part.lines.add(line);
+
+        NodeList nodeList = el.getChildNodes();
+        for (int i = 0; i < nodeList.getLength(); ++i) {
+            Node node = nodeList.item(i);
+            if (Node.ELEMENT_NODE == node.getNodeType()) {
+                Element child = (Element) node;
+                String className = child.getAttribute("class");
+                if (className.equals("ocrx_word")) {
+                    parseWord(line, child);
+                }
+            }
+        }
+
+        return line;
+    }
+
+    private Word parseWord(Line line, Element el) {
+        Word word = new Word();
+
+        word.word = el.getTextContent();
+
+        String title = el.getAttribute("title");
+        String[] params = title.split(";");
+        for (String param : params) {
+            param = param.trim();
+            if (param.startsWith("bbox")) {
+                String[] bbParams = param.split(" ");
+                BoundingBox bbox = new BoundingBox(Integer.parseInt(bbParams[1]),
+                        Integer.parseInt(bbParams[2]),
+                        Integer.parseInt(bbParams[3]),
+                        Integer.parseInt(bbParams[4]));
+                word.bbox = bbox;
+
+                break;
+            }
+        }
+
+        line.words.add(word);
+
+        return word;
     }
 
     public class Page {
-
         protected BoundingBox bbox;
+
+        protected List<Area> areas = new ArrayList<>();
 
         protected Page() {
         }
 
+        public JSONObject toJSON() {
+            JSONObject json = new JSONObject();
+            return json;
+        }
     }
 
     public class Area {
+        protected BoundingBox bbox;
 
+        protected List<Part> parts = new ArrayList<>();
+
+        protected Area() {
+        }
+
+        public JSONObject toJSON() {
+            JSONObject json = new JSONObject();
+            return json;
+        }
+    }
+
+    public class Part {
+        protected BoundingBox bbox;
+
+        protected String language;
+
+        protected List<Line> lines = new ArrayList<>();
+
+        protected Part() {
+        }
+
+        public JSONObject toJSON() {
+            JSONObject json = new JSONObject();
+            return json;
+        }
+    }
+
+    public class Line {
+        protected BoundingBox bbox;
+
+        protected List<Word> words = new ArrayList<>();
+
+        protected Line() {
+        }
+
+        public JSONObject toJSON() {
+            JSONObject json = new JSONObject();
+            return json;
+        }
+    }
+
+    public class Word {
+        protected BoundingBox bbox;
+
+        protected String word;
+
+        protected Word() {
+        }
+
+        public JSONObject toJSON() {
+            JSONObject json = new JSONObject();
+            return json;
+        }
     }
 
     public static void main(String[] args) {
