8a9d2b582290a9159072069d57932030a11dbcc9
Ambrose Xu <xujiangwei@spap.com>
2023-03-27 10:20:22 +0800

    Update AIGC

---
+++ service/src/cube/service/aigc/AIGCCellet.java
@@ -0,0 +1,67 @@
+/*
+ * This source file is part of Cube.
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2020-2023 Cube Team.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package cube.service.aigc;
+
+import cell.core.talk.Primitive;
+import cell.core.talk.TalkContext;
+import cube.core.AbstractCellet;
+import cube.core.Kernel;
+import cube.service.auth.AuthService;
+import cube.service.robot.RobotService;
+
+/**
+ * AIGC 服务单元。
+ */
+public class AIGCCellet extends AbstractCellet {
+
+    private AIGCService service;
+
+    public AIGCCellet() {
+        super(AIGCService.NAME);
+    }
+
+    @Override
+    public boolean install() {
+        this.service = new AIGCService(this);
+
+        Kernel kernel = (Kernel) this.getNucleus().getParameter("kernel");
+        kernel.installModule(RobotService.NAME, this.service);
+
+        return true;
+    }
+
+    @Override
+    public void uninstall() {
+        Kernel kernel = (Kernel) this.getNucleus().getParameter("kernel");
+        kernel.uninstallModule(AuthService.NAME);
+    }
+
+    @Override
+    public void onListened(TalkContext talkContext, Primitive primitive) {
+        super.onListened(talkContext, primitive);
+    }
+}
