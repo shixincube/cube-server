cf6e27c2d718500ae91a2ad14954e0d09bf4fae5
Ambrose Xu <xujiangwei@spap.com>
2023-11-06 14:15:06 +0800

    Update AIGC

--- common/src/cube/aigc/psychology/Evaluation.java
+++ common/src/cube/aigc/psychology/Evaluation.java
@@ -26,11 +26,164 @@
 
 package cube.aigc.psychology;
 
+import cube.aigc.psychology.composition.FrameStructure;
+import cube.aigc.psychology.composition.SpaceLayout;
+import cube.aigc.psychology.material.House;
+import cube.aigc.psychology.material.Person;
+import cube.aigc.psychology.material.Thing;
+import cube.aigc.psychology.material.Tree;
+import cube.vision.BoundingBox;
+import cube.vision.Size;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * 评估器。
+ */
 public class Evaluation {
 
-    public Evaluation() {
+    private PaintingDescription paintingDescription;
 
+    private Size canvasSize;
+
+    private SpaceLayout spaceLayout;
+
+    public Evaluation(PaintingDescription paintingDescription) {
+        this.paintingDescription = paintingDescription;
+        this.canvasSize = paintingDescription.getCanvasSize();
     }
 
+    public FrameStructureDescription calcHouseFrameStructure() {
+        House house = null;//this.getHouse();
+        if (null == house) {
+            return null;
+        }
+        return this.calcFrameStructure(house);
+    }
 
+    public FrameStructureDescription calcTreeFrameStructure() {
+        Tree tree = null;//this.getTree();
+        if (null == tree) {
+            return null;
+        }
+        return this.calcFrameStructure(tree);
+    }
+
+    public FrameStructureDescription calcPersonFrameStructure() {
+        Person person = null;//this.getPerson();
+        if (null == person) {
+            return null;
+        }
+        return this.calcFrameStructure(person);
+    }
+
+    private FrameStructureDescription calcFrameStructure(Thing thing) {
+        int halfHeight = (int) (this.canvasSize.height * 0.5);
+        int halfWidth = (int) (this.canvasSize.width * 0.5);
+
+        BoundingBox topSpaceBox = new BoundingBox(0, 0,
+                this.canvasSize.width, halfHeight);
+        BoundingBox bottomSpaceBox = new BoundingBox(0, halfHeight,
+                this.canvasSize.width, halfHeight);
+        BoundingBox leftSpaceBox = new BoundingBox(0, 0,
+                halfWidth, this.canvasSize.height);
+        BoundingBox rightSpaceBox = new BoundingBox(halfWidth, 0,
+                halfWidth, this.canvasSize.height);
+
+        FrameStructureDescription fsd = new FrameStructureDescription();
+
+        // 判读上下空间
+        int topArea = topSpaceBox.calculateCollisionArea(thing.getBoundingBox());
+        int bottomArea = bottomSpaceBox.calculateCollisionArea(thing.getBoundingBox());
+        if (topArea > bottomArea) {
+            fsd.addFrameStructure(FrameStructure.WholeTopSpace);
+        }
+        else {
+            fsd.addFrameStructure(FrameStructure.WholeBottomSpace);
+        }
+
+        // 判断左右空间
+        int leftArea = leftSpaceBox.calculateCollisionArea(thing.getBoundingBox());
+        int rightArea = rightSpaceBox.calculateCollisionArea(thing.getBoundingBox());
+        if (leftArea > rightArea) {
+            fsd.addFrameStructure(FrameStructure.WholeLeftSpace);
+        }
+        else {
+            fsd.addFrameStructure(FrameStructure.WholeRightSpace);
+        }
+
+        // 中间区域
+        int paddingWidth = Math.round(((float) this.canvasSize.width) / 6.0f);
+        int paddingHeight = Math.round(((float) this.canvasSize.height) / 6.0f);
+        BoundingBox centerBox = new BoundingBox(paddingWidth, paddingHeight,
+                this.canvasSize.width - paddingWidth * 2,
+                this.canvasSize.height - paddingHeight * 2);
+        halfHeight = (int) (centerBox.height * 0.5);
+        halfWidth = (int) (centerBox.width * 0.5);
+        BoundingBox topLeftBox = new BoundingBox(centerBox.x, centerBox.y, halfWidth, halfHeight);
+        BoundingBox topRightBox = new BoundingBox(centerBox.x + halfWidth, centerBox.y,
+                halfWidth, halfHeight);
+        BoundingBox bottomLeftBox = new BoundingBox(centerBox.x, centerBox.y + halfHeight,
+                halfWidth, halfHeight);
+        BoundingBox bottomRightBox = new BoundingBox(centerBox.x + halfWidth, centerBox.y + halfHeight,
+                halfWidth, halfHeight);
+        int topLeftArea = topLeftBox.calculateCollisionArea(thing.getBoundingBox());
+        int topRightArea = topRightBox.calculateCollisionArea(thing.getBoundingBox());
+        int bottomLeftArea = bottomLeftBox.calculateCollisionArea(thing.getBoundingBox());
+        int bottomRightArea = bottomRightBox.calculateCollisionArea(thing.getBoundingBox());
+
+        List<AreaDesc> list = new ArrayList<>(4);
+        list.add(new AreaDesc(topLeftArea, FrameStructure.CenterTopLeftSpace));
+        list.add(new AreaDesc(topRightArea, FrameStructure.CenterTopRightSpace));
+        list.add(new AreaDesc(bottomLeftArea, FrameStructure.CenterBottomLeftSpace));
+        list.add(new AreaDesc(bottomRightArea, FrameStructure.CenterBottomRightSpace));
+
+        // 面积从小到达排列
+        Collections.sort(list, new Comparator<AreaDesc>() {
+            @Override
+            public int compare(AreaDesc ad1, AreaDesc ad2) {
+                return ad1.area - ad2.area;
+            }
+        });
+
+        fsd.addFrameStructure(list.get(list.size() - 1).structure);
+        return fsd;
+    }
+
+    public class FrameStructureDescription {
+
+        private List<FrameStructure> frameStructures;
+
+        private FrameStructureDescription() {
+            this.frameStructures = new ArrayList<>();
+        }
+
+        protected void addFrameStructure(FrameStructure structure) {
+            if (this.frameStructures.contains(structure)) {
+                return;
+            }
+            this.frameStructures.add(structure);
+        }
+
+        public boolean isWholeTop() {
+            return this.frameStructures.contains(FrameStructure.WholeTopSpace);
+        }
+
+        public boolean isWholeBottom() {
+            return this.frameStructures.contains(FrameStructure.WholeBottomSpace);
+        }
+    }
+
+    private class AreaDesc {
+        protected int area;
+        protected FrameStructure structure;
+
+        protected AreaDesc(int area, FrameStructure structure) {
+            this.area = area;
+            this.structure = structure;
+        }
+    }
 }
--- common/src/cube/aigc/psychology/PaintingDescription.java
+++ common/src/cube/aigc/psychology/PaintingDescription.java
@@ -27,7 +27,6 @@
 package cube.aigc.psychology;
 
 import cell.util.log.Logger;
-import cube.aigc.psychology.composition.FrameStructure;
 import cube.aigc.psychology.material.*;
 import cube.aigc.psychology.material.house.*;
 import cube.aigc.psychology.material.person.*;
@@ -503,6 +502,10 @@ public class PaintingDescription implements JSONable {
         return result;
     }
 
+    public Size getCanvasSize() {
+        return this.canvasSize;
+    }
+
     public void addHouse(House house) {
         if (null == this.houseList) {
             this.houseList = new ArrayList<>();
@@ -645,103 +648,6 @@ public class PaintingDescription implements JSONable {
         return bbdList;
     }
 
-    public FrameStructureDescription calcHouseFrameStructure() {
-        House house = this.getHouse();
-        if (null == house) {
-            return null;
-        }
-        return this.calcFrameStructure(house);
-    }
-
-    public FrameStructureDescription calcTreeFrameStructure() {
-        Tree tree = this.getTree();
-        if (null == tree) {
-            return null;
-        }
-        return this.calcFrameStructure(tree);
-    }
-
-    public FrameStructureDescription calcPersonFrameStructure() {
-        Person person = this.getPerson();
-        if (null == person) {
-            return null;
-        }
-        return this.calcFrameStructure(person);
-    }
-
-    private FrameStructureDescription calcFrameStructure(Thing thing) {
-        int halfHeight = (int) (this.canvasSize.height * 0.5);
-        int halfWidth = (int) (this.canvasSize.width * 0.5);
-
-        BoundingBox topSpaceBox = new BoundingBox(0, 0,
-                this.canvasSize.width, halfHeight);
-        BoundingBox bottomSpaceBox = new BoundingBox(0, halfHeight,
-                this.canvasSize.width, halfHeight);
-        BoundingBox leftSpaceBox = new BoundingBox(0, 0,
-                halfWidth, this.canvasSize.height);
-        BoundingBox rightSpaceBox = new BoundingBox(halfWidth, 0,
-                halfWidth, this.canvasSize.height);
-
-        FrameStructureDescription fsd = new FrameStructureDescription();
-
-        // 判读上下空间
-        int topArea = topSpaceBox.calculateCollisionArea(thing.getBoundingBox());
-        int bottomArea = bottomSpaceBox.calculateCollisionArea(thing.getBoundingBox());
-        if (topArea > bottomArea) {
-            fsd.addFrameStructure(FrameStructure.WholeTopSpace);
-        }
-        else {
-            fsd.addFrameStructure(FrameStructure.WholeBottomSpace);
-        }
-
-        // 判断左右空间
-        int leftArea = leftSpaceBox.calculateCollisionArea(thing.getBoundingBox());
-        int rightArea = rightSpaceBox.calculateCollisionArea(thing.getBoundingBox());
-        if (leftArea > rightArea) {
-            fsd.addFrameStructure(FrameStructure.WholeLeftSpace);
-        }
-        else {
-            fsd.addFrameStructure(FrameStructure.WholeRightSpace);
-        }
-
-        // 中间区域
-        int paddingWidth = Math.round(((float) this.canvasSize.width) / 6.0f);
-        int paddingHeight = Math.round(((float) this.canvasSize.height) / 6.0f);
-        BoundingBox centerBox = new BoundingBox(paddingWidth, paddingHeight,
-                this.canvasSize.width - paddingWidth * 2,
-                this.canvasSize.height - paddingHeight * 2);
-        halfHeight = (int) (centerBox.height * 0.5);
-        halfWidth = (int) (centerBox.width * 0.5);
-        BoundingBox topLeftBox = new BoundingBox(centerBox.x, centerBox.y, halfWidth, halfHeight);
-        BoundingBox topRightBox = new BoundingBox(centerBox.x + halfWidth, centerBox.y,
-                halfWidth, halfHeight);
-        BoundingBox bottomLeftBox = new BoundingBox(centerBox.x, centerBox.y + halfHeight,
-                halfWidth, halfHeight);
-        BoundingBox bottomRightBox = new BoundingBox(centerBox.x + halfWidth, centerBox.y + halfHeight,
-                halfWidth, halfHeight);
-        int topLeftArea = topLeftBox.calculateCollisionArea(thing.getBoundingBox());
-        int topRightArea = topRightBox.calculateCollisionArea(thing.getBoundingBox());
-        int bottomLeftArea = bottomLeftBox.calculateCollisionArea(thing.getBoundingBox());
-        int bottomRightArea = bottomRightBox.calculateCollisionArea(thing.getBoundingBox());
-
-        List<AreaDesc> list = new ArrayList<>(4);
-        list.add(new AreaDesc(topLeftArea, FrameStructure.CenterTopLeftSpace));
-        list.add(new AreaDesc(topRightArea, FrameStructure.CenterTopRightSpace));
-        list.add(new AreaDesc(bottomLeftArea, FrameStructure.CenterBottomLeftSpace));
-        list.add(new AreaDesc(bottomRightArea, FrameStructure.CenterBottomRightSpace));
-
-        // 面积从小到达排列
-        Collections.sort(list, new Comparator<AreaDesc>() {
-            @Override
-            public int compare(AreaDesc ad1, AreaDesc ad2) {
-                return ad1.area - ad2.area;
-            }
-        });
-
-        fsd.addFrameStructure(list.get(list.size() - 1).structure);
-        return fsd;
-    }
-
     private Thing getMaxArea(List<? extends Thing> list) {
         Thing maxAreaThing = null;
         int area = 0;
@@ -854,40 +760,6 @@ public class PaintingDescription implements JSONable {
     @Override
     public JSONObject toCompactJSON() {
         return this.toJSON();
-    }
-
-    public class FrameStructureDescription {
-
-        private List<FrameStructure> frameStructures;
-
-        private FrameStructureDescription() {
-            this.frameStructures = new ArrayList<>();
-        }
-
-        protected void addFrameStructure(FrameStructure structure) {
-            if (this.frameStructures.contains(structure)) {
-                return;
-            }
-            this.frameStructures.add(structure);
-        }
-
-        public boolean isWholeTop() {
-            return this.frameStructures.contains(FrameStructure.WholeTopSpace);
-        }
-
-        public boolean isWholeBottom() {
-            return this.frameStructures.contains(FrameStructure.WholeBottomSpace);
-        }
-    }
-
-    private class AreaDesc {
-        protected int area;
-        protected FrameStructure structure;
-
-        protected AreaDesc(int area, FrameStructure structure) {
-            this.area = area;
-            this.structure = structure;
-        }
     }
 
     private class BoundingBoxComparator implements Comparator<Thing> {
